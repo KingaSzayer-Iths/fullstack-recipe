b0e4ded521acbfc8e89883a961a136b1483fc434
Kinga Szayer <kinga.szayer@iths.se>
2022-05-23 22:12:31 +0200

    Add dotenv

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,9 @@ import express, { Request, Response, json } from 'express'
 import { connect } from 'mongoose'
 import recipeRouter from './routes/recipeRouter'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 app.use(json())
@@ -17,9 +20,11 @@ const port = process.env.PORT || 4000
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello there!')
 })
-
-connect('mongodb+srv://KingaSzayer-Iths:bpja5dUaeZal7qYG@cluster0.s0m39.mongodb.net/recipe?retryWrites=true&w=majority').then(() => {
-    app.listen(port, () => {
-        console.log(`App listening on port ${port}`)
-    })
-})
+if (process.env.MONGO_DB_CONNECTION_STRING){
+    connect(process.env.MONGO_DB_CONNECTION_STRING).then(() => {
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`)
+        })
+})} else {
+    console.log('Configuration process.env.MONGO_DB_CONNECTION_STRING not set')
+}
